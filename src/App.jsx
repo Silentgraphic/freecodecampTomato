bff6e41e5e38efd12cb082a43f4a0a54a04fe10c
silentgraphic <kierannolan1995@gmail.com>
2024-11-05 11:01:28 +0000

    Setup basic buttons and timer state

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
+import { useState } from "react";
+import Buttons from "./Buttons";
+
 function App() {
+  const [sesionLength, setSession] = useState(25);
+  const [breakLength, setBreak] = useState(5);
 
   return (
-    <div>
-
+    <div id="clock">
+      <Buttons
+        sesionLength={sesionLength}
+        setSession={setSession}
+        breakLength={breakLength}
+        setBreak={setBreak}
+      />
     </div>
   );
 }
