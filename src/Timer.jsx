532c037adc0479bea6b31ab6ff8cd0eafcc96052
silentgraphic <kierannolan1995@gmail.com>
2024-11-12 12:12:11 +0000

    Fixed reset logic

--- src/Timer.jsx
+++ src/Timer.jsx
@@ -8,37 +8,40 @@ function Timer({
     setSOrB,
     isRunning
 }) {
-
     const timer = useRef();
-    const [seconds, setSeconds] = useState(0);
+    let [time, setTime] = useState([sesionLength, 0]);
+
+    useEffect(() => {
+        if (isRunning) {
+            timer.current = setInterval(() => {
+                setTime(([min, sec]) => sec === 0 ? [min - 1, 59] : [min, sec - 1]);
+            }, 1000);
+        } else {
+            clearInterval(timer.current);
+        }
+    }, [isRunning]);
+
+
+    useEffect(() => {
+        //If time reaches zero swap to Session to break
+        if (time[0] === 0 && time[1] === 0) setSOrB(sOrB === "Session" ? "Break" : "Session");
+    }, [time]);
+
+    useEffect(() => {
+        //Check if session or break and set time appropriately 
+        setTime(sOrB === "Session" ? [sesionLength, 0] : [breakLength, 0]);
+    }, [sOrB, isRunning]);
+
 
     const convertToString = () => {
-        let minsString = "";
-        let secsString = seconds.toString();
-
-        if (sOrB === "Session") minsString = sesionLength.toString();
-        else if (sOrB === "Break") minsString = breakLength.toString();
+        let minsString = time[0].toString();
+        let secsString = time[1].toString();
 
         if (minsString.length === 1) minsString = "0" + minsString;
         if (secsString.length === 1) secsString = "0" + secsString;
 
         return `${minsString}:${secsString}`;
     };
-
-    useEffect(() => {
-        if (isRunning) {
-            timer.current = setInterval(() => {
-                setSeconds((e) => {
-                    if (e === 0) {
-                        return 59;
-                    }
-                    else return e - 1;
-                });
-            }, 1000);
-        } else {
-            clearInterval(timer.current);
-        }
-    }, [isRunning]);
 
     return (
         <div id="time-left">
